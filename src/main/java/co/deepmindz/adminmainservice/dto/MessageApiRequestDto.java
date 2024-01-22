584f2ee3e0b6b4c0cb79ff292789b014e4c88474
Neeraj kumar Napit <95521386+Neerajsaxenaa@users.noreply.github.com>
2024-01-22 13:17:44 +0530

    crash fixing

--- src/main/java/co/deepmindz/adminmainservice/dto/MessageApiRequestDto.java
+++ src/main/java/co/deepmindz/adminmainservice/dto/MessageApiRequestDto.java
@@ -1,26 +1,26 @@
-package co.deepmindz.adminmainservice.dto;
-
-import org.json.simple.JSONObject;
-
-import com.fasterxml.jackson.databind.util.JSONPObject;
-
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class MessageApiRequestDto {
-
-	@NotNull
-	@NotEmpty
-	private String api;
-
-	@NotNull
-	@NotEmpty
-	private JSONObject apiJson;
-
-}
+//package co.deepmindz.adminmainservice.dto;
+//
+//import org.json.simple.JSONObject;
+//
+//import com.fasterxml.jackson.databind.util.JSONPObject;
+//
+//import jakarta.validation.constraints.NotEmpty;
+//import jakarta.validation.constraints.NotNull;
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
+//public class MessageApiRequestDto {
+//
+//	@NotNull
+//	@NotEmpty
+//	private String api;
+//
+//	@NotNull
+//	@NotEmpty
+//	private JSONObject apiJson;
+//
+//}
